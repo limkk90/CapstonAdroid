12631ef8247d59e623e0a9ca948d9afdf15fba5a
limkk90 <44752388+limkk90@users.noreply.github.com>
2021-04-22 19:34:01 +0900

    Make SetFrag

--- app/src/main/java/org/yju/myapplication/CommunityAcitivty.java
+++ app/src/main/java/org/yju/myapplication/CommunityAcitivty.java
@@ -1,11 +1,13 @@
 package org.yju.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -29,9 +31,32 @@ public class CommunityAcitivty extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_community_acitivty);
 
-
-
-
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.action_notice:
+                        setFrag(0);
+                        break;
+                    case R.id.action_freeBoard:
+                        setFrag(1);
+                        break;
+                    case R.id.action_tip:
+                        setFrag(2);
+                        break;
+                    case R.id.action_news:
+                        setFrag(3);
+                        break;
+                }
+                return true;
+            }
+        });
+        cm_notice = new CommunityNoticeFrag();
+        cm_free = new CommunityFreeFrag();
+        cm_tip = new CommunityTipFrag();
+        cm_news = new CommunityNewsFrag();
+        setFrag(0);
 //        //네비게이션 변수====================================
 //        iv_menu = (ImageView)findViewById(R.id.iv_menu);
 //        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
@@ -43,6 +68,10 @@ public class CommunityAcitivty extends AppCompatActivity {
 //                drawerLayout.openDrawer(drawerView);
 //            }
 //        });
+    }
+
+    private void setFrag(int n){
+        
     }
 
 
