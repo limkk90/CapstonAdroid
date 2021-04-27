ea3412186abe95fbbc500fc9e305e6fdee2c9db9
limkk90 <44752388+limkk90@users.noreply.github.com>
2021-04-27 19:25:07 +0900

    recyclerView Success

---
+++ app/src/main/java/org/yju/myapplication/CommunityAdapter.java
@@ -0,0 +1,73 @@
+package org.yju.myapplication;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.yju.myapplication.data.Board;
+
+import java.util.ArrayList;
+
+public class CommunityAdapter extends RecyclerView.Adapter<CommunityAdapter.ViewHolder> {
+    private ArrayList<Board> mData = null;
+
+    CommunityAdapter(ArrayList<Board> list){
+        mData = list;
+        Log.i("왜", mData.toString());
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView title, content, date, writer;
+
+        ViewHolder(View itemView){
+            super(itemView);
+
+            title = itemView.findViewById(R.id.title_list);
+            content = itemView.findViewById(R.id.content_list);
+            date = itemView.findViewById(R.id.date_list);
+            writer = itemView.findViewById(R.id.writer_list);
+        }
+    }
+
+    @NonNull
+    @Override
+    public CommunityAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.community_item, parent, false);
+        CommunityAdapter.ViewHolder vh = new CommunityAdapter.ViewHolder(view);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Board item = mData.get(position);
+
+        holder.title.setText(item.getB_title());
+        holder.content.setText(item.getB_content());
+        holder.writer.setText(item.getU_id());
+//        holder.date.setText(item.getB_dtt().toString());
+
+        holder.title.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
+        holder.content.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
+        holder.writer.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
+//        holder..setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
+        holder.title.setGravity(Gravity.CENTER);
+        holder.content.setGravity(Gravity.CENTER);
+        holder.writer.setGravity(Gravity.CENTER);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+}
--- app/src/main/java/org/yju/myapplication/CommunityFreeFrag.java
+++ app/src/main/java/org/yju/myapplication/CommunityFreeFrag.java
@@ -8,20 +8,69 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import org.yju.myapplication.R;
+import org.yju.myapplication.data.Board;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class CommunityFreeFrag extends Fragment {
     private View view;
+    RecyclerView recyclerView = null;
+    CommunityAdapter adapter = null;
+    ArrayList<Board> bList = new ArrayList<Board>();
+    LinearLayoutManager linearLayoutManager = null;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.community_free, container, false);
 
+        recyclerView = view.findViewById(R.id.recyclerView);
 
+        adapter = new CommunityAdapter(bList);
+        recyclerView.setAdapter(adapter);
+        linearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), linearLayoutManager.getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
+        addItem("김", "김", "임");
+        addItem("김", "김", "임");
+        addItem("김", "김", "임");
+        addItem("김", "김", "임");
+        addItem("김", "김", "임");
+        addItem("김", "김", "임");
+        addItem("김", "김", "임");
+        addItem("김", "김", "임");
+        addItem("김", "김", "임");
+        addItem("김", "김", "임");
+        addItem("김", "김", "임");
+        addItem("김", "김", "임");
+        addItem("김", "김", "임");
+        addItem("김", "김", "임");
+        addItem("김", "김", "임");
+        addItem("김", "김", "임");
+        addItem("김", "김", "임");
+        addItem("김", "김", "임");
+        addItem("김", "김", "임");
+        addItem("김", "김", "임");
+        addItem("김", "김", "임");
+        adapter.notifyDataSetChanged();
         return view;
 
 
+    }
+
+    public void addItem(String title, String content, String writer){
+        Board item = new Board();
+        item.setB_title(title);
+        item.setB_content(content);
+        item.setU_id(writer);
+        bList.add(item);
     }
 }
