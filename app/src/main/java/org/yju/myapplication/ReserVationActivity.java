e01b1ff18758a429e71cd1f041ee44864cd9e39f
limkk90 <44752388+limkk90@users.noreply.github.com>
2021-05-21 18:04:04 +0900

    spinner success

--- app/src/main/java/org/yju/myapplication/ReserVationActivity.java
+++ app/src/main/java/org/yju/myapplication/ReserVationActivity.java
@@ -19,6 +19,9 @@ public class ReserVationActivity extends AppCompatActivity {
                      "18", "19", "20", "21", "22", "23"};
     String minute[] = {"00", "10", "20", "30", "40", "50"};
 
+    String hourEnd[] = {"00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17",
+            "18", "19", "20", "21", "22", "23"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,24 +29,19 @@ public class ReserVationActivity extends AppCompatActivity {
 
         spinnerStartH = findViewById(R.id.startSpinnerKara);
         spinnerStartM = findViewById(R.id.startSpinnerMade);
-        spinnerEndH = findViewById(R.id.startSpinnerKara);
-        spinnerEndM = findViewById(R.id.startSpinnerMade);
+        spinnerEndH = findViewById(R.id.endSpinnerKara);
+        spinnerEndM = findViewById(R.id.endSpinnerMade);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, hour);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerStartH.setAdapter(adapter);
-
-
-        ArrayAdapter<String> minAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, minute);
-        minAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerStartM.setAdapter(minAdapter);
+        ArrayAdapter<String> startHouradapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, hour);
+        startHouradapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerStartH.setAdapter(startHouradapter);
+        spinnerEndH.setAdapter(startHouradapter);
 
 
-        ArrayAdapter<String> endHourAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, hour);
-        endHourAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-
-
+        ArrayAdapter<String> startMinAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, minute);
+        startMinAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerStartM.setAdapter(startMinAdapter);
+        spinnerEndM.setAdapter(startMinAdapter);
 
         spinnerStartH.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -70,7 +68,29 @@ public class ReserVationActivity extends AppCompatActivity {
             }
         });
 
+        spinnerEndH.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "선택된 아이템:"+ spinnerEndH.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spinnerEndM.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "선택된 아이템:"+ spinnerEndM.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
     }
 }
