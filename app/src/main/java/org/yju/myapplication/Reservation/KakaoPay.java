154a6c7ad3d6cb34c20f0a329b37f5c39d1a8bd4
limkk90 <44752388+limkk90@users.noreply.github.com>
2021-07-20 16:46:32 +0900

    add kakaoPay

---
+++ app/src/main/java/org/yju/myapplication/Reservation/KakaoPay.java
@@ -0,0 +1,102 @@
+package org.yju.myapplication.Reservation;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import org.yju.myapplication.R;
+
+import kr.co.bootpay.Bootpay;
+import kr.co.bootpay.BootpayAnalytics;
+import kr.co.bootpay.enums.Method;
+import kr.co.bootpay.enums.PG;
+import kr.co.bootpay.enums.UX;
+import kr.co.bootpay.listener.CancelListener;
+import kr.co.bootpay.listener.CloseListener;
+import kr.co.bootpay.listener.ConfirmListener;
+import kr.co.bootpay.listener.DoneListener;
+import kr.co.bootpay.listener.ErrorListener;
+import kr.co.bootpay.listener.ReadyListener;
+import kr.co.bootpay.model.BootExtra;
+import kr.co.bootpay.model.BootUser;
+
+public class KakaoPay extends AppCompatActivity {
+    private int stuck = 10;
+    Button btn_kakaoPay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kakao_pay);
+        btn_kakaoPay = findViewById(R.id.btn_kakaoPay);
+
+    }
+
+
+    public void onClick_request(View v){
+        BootUser bootUser = new BootUser().setPhone("010-1234-5678");
+        BootExtra bootExtra = new BootExtra().setQuotas(new int[] {0,2,3});
+
+        Bootpay.init(getFragmentManager())
+                .setApplicationId("60f670177b5ba4001f1de8fc")
+                .setPG(PG.INICIS) // 결제할 PG 사
+                .setMethod(Method.KAKAO) //결제수단
+                .setContext(this)
+                .setBootUser(bootUser)
+                .setBootExtra(bootExtra)
+                .setUX(UX.PG_DIALOG)
+                .setName("전기차 충전소 결제금액") //결제할 상품명
+                .setOrderId("1234") //결제 고유번호expire_month
+                .setPrice(500) // 결제할 금액
+                .addItem("마우's 스", 1, "ITEM_CODE_MOUSE", 100) // 주문정보에 담길 상품정보, 통계를 위해 사용
+                .addItem("키보드", 1, "ITEM_CODE_KEYBOARD", 200, "패션", "여성상의", "블라우스") // 주문정보에 담길 상품정보, 통계를 위해 사용
+                .onConfirm(new ConfirmListener() { // 결제가 진행되기 바로 직전 호출되는 함수로, 주로 재고처리 등의 로직이 수행
+                    @Override
+                    public void onConfirm(@Nullable String message) {
+
+                        if (0 < stuck) Bootpay.confirm(message); // 재고가 있을 경우.
+                        else Bootpay.removePaymentWindow(); // 재고가 없어 중간에 결제창을 닫고 싶을 경우
+                        Log.d("confirm", message);
+                    }
+                })
+                .onDone(new DoneListener() { // 결제완료시 호출, 아이템 지급 등 데이터 동기화 로직을 수행합니다
+                    @Override
+                    public void onDone(@Nullable String message) {
+                        Log.d("done", message);
+                    }
+                })
+                .onReady(new ReadyListener() { // 가상계좌 입금 계좌번호가 발급되면 호출되는 함수입니다.
+                    @Override
+                    public void onReady(@Nullable String message) {
+                        Log.d("ready", message);
+                    }
+                })
+                .onCancel(new CancelListener() { // 결제 취소시 호출
+                    @Override
+                    public void onCancel(@Nullable String message) {
+
+                        Log.d("cancel", message);
+                    }
+                })
+                .onError(new ErrorListener() { // 에러가 났을때 호출되는 부분
+                    @Override
+                    public void onError(@Nullable String message) {
+                        Log.d("error", message);
+                    }
+                })
+                .onClose(
+                        new CloseListener() { //결제창이 닫힐때 실행되는 부분
+                            @Override
+                            public void onClose(String message) {
+                                Log.d("close", "close");
+                            }
+                        })
+                        .request();
+    }
+
+}
--- app/src/main/java/org/yju/myapplication/Reservation/ReservationApi.java
+++ app/src/main/java/org/yju/myapplication/Reservation/ReservationApi.java
@@ -18,9 +18,11 @@ public interface ReservationApi {
     @GET("api/{stat_id}/chargerlist")
     Call<List<Charger>> getCharger(@Path("stat_id") String stat_id);
 
+    //예약하기
     @POST("api/reservation")
     Call<Boolean> insertRsvt(@Body Rsvt rsvt);
 
+    //충전 싹 다 조회
     @GET("api/todays-reservation/{chg_id}/{stat_id}")
     Call<List<Rsvt>> getTodaysRsvt(@Path("chg_id") String chg_id, @Path("stat_id") String stat_id);
 
