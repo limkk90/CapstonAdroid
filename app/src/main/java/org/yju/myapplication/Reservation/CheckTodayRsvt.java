5c12eb6c281e13bfdd9c764e9815f544986b51b6
limkk90 <44752388+limkk90@users.noreply.github.com>
2021-07-22 09:40:34 +0900

    add CheckTodayRsvt

--- app/src/main/java/org/yju/myapplication/Reservation/CheckTodayRsvt.java
+++ app/src/main/java/org/yju/myapplication/Reservation/CheckTodayRsvt.java
@@ -1,11 +1,14 @@
 package org.yju.myapplication.Reservation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
+import org.yju.myapplication.Community.CommunityAdapter;
 import org.yju.myapplication.DataService;
 import org.yju.myapplication.R;
 import org.yju.myapplication.data.Rsvt;
@@ -17,11 +20,25 @@ public class CheckTodayRsvt extends AppCompatActivity {
     RecyclerView recyclerView=null;
     ArrayList<Rsvt> rsvtList = new ArrayList<>();
     CheckTodayRsvtAdapter adapter = null;
+    LinearLayoutManager linearLayoutManager = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_today_rsvt2);
+
+        //리사이클러뷰 처리;
+        recyclerView = findViewById(R.id.check_recyclerVIew);
+        adapter = new CheckTodayRsvtAdapter(rsvtList);
+        recyclerView.setAdapter(adapter);
+        linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), linearLayoutManager.getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
+
+
+        adapter.notifyDataSetChanged();
 
 
     }
